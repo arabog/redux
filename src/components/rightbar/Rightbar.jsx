a803cadbb54e3741fcdcdabe3f245452a86dc129
iCode_Rx <arababs2015@gmail.com>
2021-10-04 21:29:55 +0100

    recommendation & rightbar added

--- src/components/rightbar/Rightbar.jsx
+++ src/components/rightbar/Rightbar.jsx
@@ -1,11 +1,23 @@
 import React from 'react'
+import "./rightbar.css"
+import Recommendation from '../recommendation/Recommendation'
+import { ArrowDropDown } from "@material-ui/icons"
 
 export default function Rightbar() {
 
           
           return (
                     <div className="rightbar">
-                              Right bar
+                              <div className="rightbarWrapper">
+                                        <Recommendation type= "user" />
+                                        <Recommendation type="popular" />
+                                        <Recommendation type="editor" />
+
+                                        <button className="rightButton">
+                                                  See More
+                                                  <ArrowDropDown />
+                                        </button>
+                              </div>
                     </div>
           )
 }
